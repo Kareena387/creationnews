70e2cf65a4a6622613b5b6b17b21c32ea422ba07
kareena <angewills387@gmail.com>
2023-11-14 23:40:10 +0545

    added footer

--- footer.php
+++ footer.php
@@ -6,6 +6,33 @@
     </div>
 </div>
 
+
+<footer class="bg-dark text-white pt-4">
+    <div class="container text-center text-md-left">
+        <div class="row text-center text-md-left">
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Company Name</h5>
+                <p>Cre@tionNews</p>
+            </div>
+            <div class="col-md-2 col-xl-2 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-warning">contact us</h5>
+                <p>
+                    <a href="#" class="text-white" style="text-decoratoon: none;">Gmail</a>
+                </p>
+                <p>
+                    <a href="#" class="text-white" style="text-decoratoon: none;">Linkdln</a>
+                </p>
+                <p>
+                    <a href="#" class="text-white" style="text-decoratoon: none;">Facebook</a>
+                </p>
+                <p>
+                    <a href="#" class="text-white" style="text-decoratoon: none;">Instragram</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+
 <script src="./js/bootstrap.bundle.js"></script>
 
 </body>
